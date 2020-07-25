4e64d286c4d090a306d839d3fb91c2032ee75172
Yu Tanaka <YRK00337@nifty.com>
2020-07-25 11:31:12 +0900

    衝突解消

--- Assets/Hensu.cs
+++ Assets/Hensu.cs
@@ -4,35 +4,10 @@ using System.Collections.Generic;
 
 public class Hensu : UnityEngine.MonoBehaviour
 {
-    int vx = 20;
-
-    // 順次処理、分岐処理、繰り返し処理
-    public void ensyu71()
-    {
-        int vx = 5;
-        UnityEngine.Debug.Log(vx);
-
-        vx = 10;
-        UnityEngine.Debug.Log(vx);
-    }
-
-    public void ensyu72()
-    {
-        UnityEngine.Debug.Log(vx);
-    }
-
-    public void ensyu74()
-    {
-        vx += 10;
-        UnityEngine.Debug.Log(vx);
-    }
-
-
-    int a = 10;
-
     // Start is called before the first frame update
-    public void Proc()
+    void Start()
     {
+        int a = 10;
         float fw = 3.14f;
         string str = "こんにちは";
         bool bl = true;
@@ -51,6 +26,6 @@ public class Hensu : UnityEngine.MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
